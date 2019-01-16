d5ef2b390899a47dc6755b0909ea1da778fdc4c9
rakasarla <rakasarla@yahoo.com>
2019-01-16 14:31:07 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFileSync('server.log', log + '\n');
+    next();
+});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         pageTitle: 'Maintenance Mode',
+//         pageBody: 'Site is in Maintenance mode, please visit later'
+//     });
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('upperCase', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Get: Hello Express!</h1>');
+    // res.send({
+    //     name: 'Ravi',
+    //     likes: [
+    //         'Running',
+    //         'Swimming',
+    //         'Spinning',
+    //         'Skiing'
+    //     ]
+    // })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        pageBody: 'My First Home Page'
+    });
+
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    var errorMessage = ({
+        errorCode: -2000,
+        errorMessage: 'Bad Request',
+        errorDetails: 'Cannot figure out what the error is',
+        errorPlace: 'server.js: line 28'
+    });
+    res.send(errorMessage);
+});
+
+
+app.post('/', (req, res) => {
+    res.send('<h1>Post: Hello Express!</h1>');
+});
+
+app.listen(3000, () => {
+    console.log('Starting Server on port 3000...');
+});
+
