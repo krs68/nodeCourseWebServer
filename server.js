b9e31a778f1f903ad3f3689af8c6d553dfaf319c
rakasarla <rakasarla@yahoo.com>
2019-01-16 21:44:02 -0500

    Setup for Heroku: start script and Heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT | 3000;
 
 var app = express();
 
@@ -69,7 +70,7 @@ app.post('/', (req, res) => {
     res.send('<h1>Post: Hello Express!</h1>');
 });
 
-app.listen(3000, () => {
-    console.log('Starting Server on port 3000...');
+app.listen(port, () => {
+    console.log(`Starting Server on port ${port}...`);
 });
 
